c263862badfd501d1ef272350554db16d867407b
Bruno Maia <brunomaiadesenv@gmail.com>
2026-05-06 20:50:20 -0300

    criando atributos da classe LocalizacaoGeo

--- ServicoInteligenteGeografico/Models/LocalizacaoGeo.cs
+++ ServicoInteligenteGeografico/Models/LocalizacaoGeo.cs
@@ -8,5 +8,14 @@ namespace ServicoInteligenteGeografico.Models
 {
     public class LocalizacaoGeo
     {
+        public int Id { get; set; } 
+        public string Nome { get; set; } = string.Empty;
+        public string Numero { get; set; } = string.Empty;
+        public string Bairro {  get; set; } = string.Empty;
+        public string Cep { get; set; } = string.Empty;
+        public double Latitute { get; set; }    
+        public double Longitute { get; set; }
+        public DateTime TimeStamp { get; set; } 
+
     }
 }
